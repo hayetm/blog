5aba1b4f5204a64ed91468a4d948260848b3acac
hayetm <hayat.mrabo@gmail.com>
2017-11-07 17:21:25 +0100

    first commit, routes and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { HomeComponent } from './home/home.component';
+
+export const ROUTES: Routes = [
+  { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+        { path: 'articles', component: ArticlesComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ArticlesComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+        RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
